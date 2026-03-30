d589555c6da7ada6fbe36746499dde7bbfb1ace4
Ardell Aghna Mahendra <ardell@Ardells-MacBook-Pro.local>
2026-03-30 18:02:54 +0700

    feat: register prodi and penugasan routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
 import accountRoutes from './account.routes';
+import prodiRoutes from './prodi.routes';
+import penugasanRoutes from './penugasan.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/accounts', accountRoutes);
+router.use('/prodi', prodiRoutes);
+router.use('/penugasan', penugasanRoutes);
 
 export default router;
