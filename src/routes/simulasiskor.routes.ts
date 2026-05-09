79507daceedf3b69e7f96b53ab30735c8831726e
drianto <andri070805@gmail.com>
2026-05-09 16:05:12 +0700

    feat: add simulasi skor route

---
+++ src/routes/simulasiskor.routes.ts
@@ -0,0 +1,18 @@
+﻿import { Router } from 'express';
+import {
+  getSimulationByProdiHandler,
+  updateSimulationQualitativeHandler,
+} from '../controllers/simulasiskor.controller';
+import { authenticate } from '../middlewares/auth.middleware';
+import { requireProdiAccess } from '../middlewares/prodi.middleware';
+import { validate } from '../middlewares/validate.middleware';
+import { updateSimulationQualitativeSchema } from '../validators/simulasiskor.validator';
+
+const router = Router();
+
+router.use(authenticate);
+
+router.get('/:prodiId', requireProdiAccess('read'), getSimulationByProdiHandler);
+router.put('/:prodiId/qualitative', requireProdiAccess('write'), validate(updateSimulationQualitativeSchema), updateSimulationQualitativeHandler);
+
+export default router;
